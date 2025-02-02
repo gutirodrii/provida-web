1250e11e675b485705bf7bb134b55da53e4a9e66
gutirodrii <gutirodriidev@gmail.com>
2025-02-02 11:59:26 +0100

    Initial commit

---
+++ src/components/CallToAction.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const CallToAction = () => {
+  return (
+    <section className="bg-gray-100 py-16">
+      <div className="container mx-auto px-4 text-center">
+        <h2 className="text-3xl font-bold mb-4">Únete a Nuestra Misión</h2>
+        <p className="text-lg mb-8">
+          Apoya nuestra causa como voluntario o haciendo una donación.
+        </p>
+        <div className="flex justify-center gap-4">
+          <a
+            href="/voluntarios"
+            className="bg-pink-500 text-white px-6 py-3 rounded-md font-semibold hover:bg-pink-600 transition"
+          >
+            Sé Voluntario
+          </a>
+          <a
+            href="/donaciones"
+            className="bg-gray-700 text-white px-6 py-3 rounded-md font-semibold hover:bg-gray-800 transition"
+          >
+            Haz una Donación
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CallToAction;
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section className="relative h-96">
+      <img
+        src="/images/hero-bebe.webp"
+        alt="Hero Image"
+        className="relative inset-0 w-full h-full object-cover"
+      />
+      <div className="absolute inset-0 bg-black/50 flex items-center justify-center">
+        <div className="text-center text-white px-4">
+          <h1 className="text-5xl font-bold mb-4">
+            Dentro de ti tienes una nueva Vida, ¡No la cortes!
+          </h1>
+          <p className="text-xl mb-8">
+            Provida estará a tu lado, te dará soluciones, te ayudará a ver
+            mejores horizontes.
+          </p>
+          <a
+            href="/necesitas-ayuda"
+            className="bg-blue-500 text-white px-6 py-3 rounded-md font-semibold hover:bg-blue-700 transition"
+          >
+            ¿Necesitas ayuda?
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
