4fbf90955646175cefb71baefbc6890e63402ab5
gutirodrii <gutirodriidev@gmail.com>
2025-02-02 13:45:37 +0100

    tamaño letras hero

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <section className="relative -top-16 h-96">
+    <section className="relative -top-16 h-120">
       <img
         src="/images/hero-bebe.webp"
         alt="Hero Image"
@@ -10,10 +10,10 @@ const HeroSection = () => {
       />
       <div className="absolute inset-0 bg-black/50 flex items-center justify-center">
         <div className="text-center text-white px-4">
-          <h1 className="text-5xl font-bold mb-4">
+          <h1 className="text-4xl md:text-5xl font-bold mb-4">
             Dentro de ti tienes una nueva Vida, ¡No la cortes!
           </h1>
-          <p className="text-xl mb-8">
+          <p className="text-lg md:text-xl mb-8">
             Provida estará a tu lado, te dará soluciones, te ayudará a ver
             mejores horizontes.
           </p>
